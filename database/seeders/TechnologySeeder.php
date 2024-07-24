5a3de0841c0db94e80c0bd858c5cc2e580637804
DanieleBernassola <danielebernassola02@gmail.com>
2024-07-24 16:17:24 +0200

    [UPD] Created seeder for technology and seeded

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +15,22 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        Schema::disableForeignKeyConstraints();
+        Technology::truncate();
+
+        $technologies = ['HTML', 'CSS', 'Javascript', 'Vue', 'Bootstrap', 'PHP', 'MySQL', 'SQL', 'Laravel'];
+
+        foreach ($technologies as $technology) {
+            $new_technology = new Technology();
+
+            $new_technology->title = $technology;
+            $new_technology->slug = Str::of($technology)->slug();
+
+            $new_technology->save();
+        }
+
+
+        Schema::enableForeignKeyConstraints();
     }
 }
